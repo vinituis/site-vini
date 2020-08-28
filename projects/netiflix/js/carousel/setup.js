dce78fc66863fe22e831e0d9f8b6992306361674
vini <vinicius.turu@gmail.com>
2020-08-27 21:19:35 -0300

    Inseri alguns projetos

---
+++ projects/netiflix/js/carousel/setup.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        350:{
+            items:2
+        },
+        450:{
+            items:3
+        },
+        650:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
